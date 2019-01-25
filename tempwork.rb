9e0ed74dddf620166d50891d228130f0d1f008e8
Tomoki Yamashita <tomorrowkey@gmail.com>
2019-01-25 09:25:20 +0900

    Add tempwork

---
+++ tempwork.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Tempwork < Formula
+  VERSION = '0.1.6'
+
+  homepage 'https://github.com/winebarrel/tempwork'
+  url "https://github.com/winebarrel/tempwork/releases/download/v#{VERSION}/tempwork-v#{VERSION}-darwin-amd64.gz"
+  sha256 '77357cb6a723059420f358582314d5c13da60595ab26431475b56d3ee5966862'
+  version VERSION
+  head 'https://github.com/winebarrel/tempwork.git', :branch => 'master'
+
+  def install
+    bin.install 'tempwork'
+  end
+end

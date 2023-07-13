6f5c1509aad971c023acad3425b618814c906dd6
Tomoki Yamashita <tomorrowkey@gmail.com>
2023-07-13 09:44:34 +0900

    Update android-screen-record to v1.2.0

--- android-screen-record.rb
+++ android-screen-record.rb
@@ -1,11 +1,11 @@
 require 'formula'
 
 class AndroidScreenRecord < Formula
-  VERSION = '1.1.0'
+  VERSION = '1.2.0'
 
   homepage 'https://github.com/tomorrowkey/android-screen-record'
   url "https://github.com/tomorrowkey/android-screen-record/archive/v#{VERSION}.tar.gz"
-  sha256 '0d9691a56e1bcb48ceb3928337cacc1c83a4ac38e07f9c3fc035e41d9ae2baba'
+  sha256 '7204992d2a9500c5bfe10e8f3a3ba43228d91b7fd58641f9cb75765592b869c5'
   version VERSION
   head 'https://github.com/tomorrowkey/android-screen-record.git', :branch => 'main'
 
